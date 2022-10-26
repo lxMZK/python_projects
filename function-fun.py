debc212d477d2e2579e198285a341823c1286bf8
lxMZK <alexmmizak@gmail.com>
2022-10-26 18:24:40 -0400

    more recursion

--- function-fun.py
+++ function-fun.py
@@ -40,11 +40,53 @@ def arb_longest_string(*str):
     return long
 
 
-arb_args('hello', 'world')
-inner_func(1, 2)
-lunch_lady('alex')
-lunch_lady('bob', 'chicken')
-print('(sum, product): ', sum_n_product(1, 2))
-alias_arb_args('hello', 'world', '2')
-print(arb_mean(1, 2, 3, 4))
-print(arb_longest_string('a','bb','ccc','dddd','e'))
+def name_args(*items):
+    key = 1
+    for i in items:
+        print(f"{key}:{i}")
+        key += 1
+
+
+def all_true(iter):
+    return all(iter)
+
+
+def one_true(iter):
+    return any(iter)
+
+
+def recursive_factorial(n):
+    if n <= 1:
+        return 1
+    else:
+        return n*recursive_factorial(n-1)
+
+
+def recursive_deduplicate(input, i=0):
+    if len(input) <= 1 or i == len(input)-1:
+        return input
+    else:
+        if input[i] == input[i+1]:
+            return recursive_deduplicate(input[0:i]+input[i+1:], i)
+        else:
+            return recursive_deduplicate(input, i+1)
+
+
+def recursive_reverse(str, i=0):
+    if len(str) == 0:
+        return ""
+    elif i == len(str)-1:
+        return str[0]
+    else:
+        return str[-1-i] + recursive_reverse(str, i+1)
+
+
+name_args('a', 'b', 'c', 'd', 'e')
+print(all_true([True, True, True]))
+print(all_true((True, False)))
+print(one_true([True, True, True]))
+print(one_true((False, False)))
+print(recursive_factorial(5))
+print(recursive_factorial(8))
+print(recursive_deduplicate("AABBCCDD"))
+print(recursive_reverse("apple"))
